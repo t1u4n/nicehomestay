c04fb6a2cdbf873be3f7320eb21038db926059bf
TianLuan99 <tian23@illinois.edu>
2022-07-21 16:49:08 +0800

    change some styles and fix some security issues

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
 
+const mongoSanitize = require("express-mongo-sanitize");
+
 const homestayRoute = require("./routes/homestay");
 const reviewRoute = require("./routes/reviews");
 const userRoute = require('./routes/users');
@@ -19,8 +21,16 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const helmet = require('helmet');
 
-mongoose.connect('mongodb://localhost:27017/great-homestay')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+//process.env.DB_URL;
+const dbUrl = "mongodb://localhost:27017/great-homestay";
+
+const MongoDBStore = require('connect-mongo')(session);
+
+mongoose.connect(dbUrl);
+// mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
@@ -37,19 +47,34 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
+
+const store = new MongoDBStore({
+    url: dbUrl,
+    secret: 'secret',
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function(e){
+    console.log("SESSION STORE ERROR", e);
+})
 
 const sessionConfig = {
+    store,
+    name: 'session',
     secret: 'secret',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
 app.use(session(sessionConfig));
 app.use(flash());
+// app.use(helmet({ contentSecurityPolicy: false }));
 
 app.use(passport.initialize());
 app.use(passport.session());
